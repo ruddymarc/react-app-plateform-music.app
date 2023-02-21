6cfe203cfded6cfb4573aefc69094a7192b0a95a
Ruddy marc <pc-de-ruddy@LAPTOP-OONQKG13.gp>
2023-02-21 12:21:05 -0400

    feat: add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   const PAGES = ['HOME_PAGE', 'SEARCH_PAGE', 'PLAYLIST_PAGE'];
@@ -8,48 +9,32 @@ function App() {
   const mainContent = (activePage === PAGES.at(0) && 'Home page content') || (activePage === PAGES.at(1) && 'Search page content') || (activePage === PAGES.at(2) && 'Playlist page content');
 
   return (
-    <div className="App">
+    <Wrapper>
       <header>
         <section>
-          <button
-            type="button"
-            className="Link"
-            onClick={() => setActivePage(PAGES.at(0))}
-          >
-            <h1>
+          <Link onClick={() => { setActivePage(PAGES.at(0)); }}>
+            <Title>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-disc" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                 <path d="M10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 4a4 4 0 0 0-4 4 .5.5 0 0 1-1 0 5 5 0 0 1 5-5 .5.5 0 0 1 0 1zm4.5 3.5a.5.5 0 0 1 .5.5 5 5 0 0 1-5 5 .5.5 0 0 1 0-1 4 4 0 0 0 4-4 .5.5 0 0 1 .5-.5z" />
               </svg>
               Spotifuz
-            </h1>
-          </button>
+            </Title>
+          </Link>
           <nav>
-            <button
-              type="button"
-              className="Link"
-              onClick={() => setActivePage(PAGES.at(0))}
-            >
+            <Link onClick={() => { setActivePage(PAGES.at(0)); }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
                 <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
               </svg>
               Home
-            </button>
-            <button
-              type="button"
-              className="Link"
-              onClick={() => setActivePage(PAGES.at(1))}
-            >
+            </Link>
+            <Link onClick={() => { setActivePage(PAGES.at(1)); }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
               </svg>
               Search
-            </button>
-            <button
-              type="button"
-              className="Link"
-              onClick={() => setActivePage(PAGES.at(2))}
-            >
+            </Link>
+            <Link onClick={() => { setActivePage(PAGES.at(2)); }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-music-note-list" viewBox="0 0 16 16">
                 <path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2z" />
                 <path fillRule="evenodd" d="M12 3v10h-1V3h1z" />
@@ -57,56 +42,113 @@ function App() {
                 <path fillRule="evenodd" d="M0 11.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 7H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 3H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z" />
               </svg>
               Playlist
-            </button>
+            </Link>
           </nav>
         </section>
-        <div className="Player">
+        <Player>
           <img src="https://via.placeholder.com/150" alt="music-img" className="Music-picture" />
           <span className="Music-author">Author</span>
-          <div className="Music-status">
+          <MusicStatus>
             0:00 min
             <div className="Music-progress-bars">
               <div className="Music-progress-bar" />
             </div>
             ?:?? min
-          </div>
-          <div className="Player-actions">
-            <button
-              type="button"
-              className="Link"
-            >
+          </MusicStatus>
+          <PlayerActions>
+            <Link>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-rewind" viewBox="0 0 16 16">
                 <path d="M9.196 8 15 4.633v6.734L9.196 8Zm-.792-.696a.802.802 0 0 0 0 1.392l6.363 3.692c.52.302 1.233-.043 1.233-.696V4.308c0-.653-.713-.998-1.233-.696L8.404 7.304Z" />
                 <path d="M1.196 8 7 4.633v6.734L1.196 8Zm-.792-.696a.802.802 0 0 0 0 1.392l6.363 3.692c.52.302 1.233-.043 1.233-.696V4.308c0-.653-.713-.998-1.233-.696L.404 7.304Z" />
               </svg>
-            </button>
-            <button
-              type="button"
-              className="Link play"
-            >
+            </Link>
+            <LinkPlay>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                 <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
               </svg>
-            </button>
-            <button
-              type="button"
-              className="Link"
-            >
+            </LinkPlay>
+            <Link>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-fast-forward" viewBox="0 0 16 16">
                 <path d="M6.804 8 1 4.633v6.734L6.804 8Zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692Z" />
                 <path d="M14.804 8 9 4.633v6.734L14.804 8Zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C8.713 12.69 8 12.345 8 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692Z" />
               </svg>
-            </button>
-          </div>
-        </div>
+            </Link>
+          </PlayerActions>
+        </Player>
       </header>
 
       <main>
         { mainContent }
       </main>
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  gap: .5em;
+  display: flex;
+  width: 100vw;
+  &> header {
+    height: 100vh;
+    width: 16em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  &> main {
+    flex: auto;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    overflow-x: scroll;
+  }
+`;
+
+const Title = styled.h1`
+  display: inline-flex;
+  align-items: center;
+  gap: .3em;
+`;
+
+const Link = styled.button`
+  gap: .5em;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  cursor: pointer;
+  border: none;
+  background: none;
+  &> svg {
+    height: 1.5em;
+    width: auto;
+  }
+`;
+
+const LinkPlay = styled(Link)`
+  &> svg {
+    height: 3em;
+    color: green;
+  }
+`;
+
+const Player = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const PlayerActions = styled.div`
+  gap: .5em;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const MusicStatus = styled.div`
+  gap: .5em;
+  display: flex;
+  justify-content: space-between;
+`;
 
 export default App;
